9d324f355813672d8890a90a82c4d7c4787688d4
akr3ch <97300177+akr3ch@users.noreply.github.com>
2023-07-15 21:18:22 +0600

    Create main.js

---
+++ iframe/main.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+  var iframe = document.createElement('iframe');
+  iframe.src = '//akr3ch.github.io/iframe';
+  iframe.style.position = 'fixed';
+  iframe.style.top = '0';
+  iframe.style.left = '0';
+  iframe.style.width = '100%';
+  iframe.style.height = '100%';
+  iframe.style.border = 'none';
+  document.body.appendChild(iframe);
+};
